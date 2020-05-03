7fa55a8678fd179f6f94550167894c381076bbfd
leviisac <45822362+leviisac@users.noreply.github.com>
2020-05-03 16:04:05 -0300

    recitation 3

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -7,32 +7,29 @@ import java.util.Map;
 //this class is responsible to light of the ambient
 public class AmbientLight {
 
-    private final double _Ka = 0.1;
+    private final double _Ka;
     Color _intensity;
 
     // ***************** Constructors ********************** //
 
     public AmbientLight(Color color, double ka) {
         super();
-        this._color = color;
-        this._ka = ka;
+        this._intensity = color;
+        this._Ka = ka;
     }
-    }
-    // ***************** Getters/Setters ********************** //
+
+    // ***************** Getters ********************** //
     public Color getColor()
     {
-        return _color;
-    }
-    public void setColor(Color color){
-        this._color = new Color (color.getRGB());
+        return _intensity;
     }
     public double getKa(){
         return _Ka;
     }
     public Color getIntensity(){
-        int r = Math.min(255,(int)(_color.getRed()*_Ka));
-        int g = Math.min(255,(int)(_color.getGreen()*_Ka));
-        int b = Math.min(255,(int)(_color.getBlue()*_Ka));
+        int r = Math.min(255,(int)(_intensity.getRed()*_Ka));
+        int g = Math.min(255,(int)(_intensity.getGreen()*_Ka));
+        int b = Math.min(255,(int)(_intensity.getBlue()*_Ka));
         return new Color(r,g,b);
     }
 }
---
+++ src/unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,74 @@
+package unittests.renderer;
+
+import org.junit.Test;
+import java.util.Random;
+
+/**
+ * Created by Moishe on 22/05/2017.
+ */
+public class ImageWriterTest {
+
+    @Test
+    public void writeImageTest(){
+
+        ImageWriter imageWriter = new ImageWriter("Image writer test", 500, 500, 1, 1);
+        Random rand = new Random();
+        for (int i = 0; i < imageWriter.getHeight(); i++){
+            for (int j = 0; j < imageWriter.getWidth(); j++)
+            {
+
+                if (i % 25 == 0 || j % 25 == 0 || i == j || i == 499 || j == 499 || i == 500 - j)
+                    imageWriter.writePixel(j, i, 0, 0, 0);  // Black
+                else
+                if(i >= 200 && i <= 300 && j >= 200 && j <= 300)
+                    imageWriter.writePixel(j, i, 255, 0, 0); // Red
+                else
+                if(i >= 150 && i <= 350 && j >= 150 && j <= 350)
+                    imageWriter.writePixel(j, i, 0, 255, 0);  // Green
+                else
+                if(i >= 100 && i <= 400 && j >= 100 && j <= 400)
+                    imageWriter.writePixel(j, i, 0, 0, 255); // Blue
+                else
+                if(i >= 50 && i <= 450 && j >= 50 && j <= 450)
+                    imageWriter.writePixel(j, i, 255, 255, 0); // Yellow
+                else
+                    imageWriter.writePixel(j, i, rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)); // Random
+            }
+        }
+
+        imageWriter.writeToimage();
+    }
+
+    @Test
+    public void writeImageFlagTest(){
+        ImageWriter imageWriter = new ImageWriter("Image writer Haiti flag test", 450, 300, 1, 1);
+        for (int i = 0; i < imageWriter.getHeight(); i++){
+            for (int j = 0; j < imageWriter.getWidth(); j++)
+            {
+                if(i>=299||i<=1||j>=449||j<=1)
+                    imageWriter.writePixel(j,i,105,105,105);
+                else
+                if(i >= 150)
+                    imageWriter.writePixel(j, i, 238, 64, 0); // Red
+                else
+                    imageWriter.writePixel(j, i, 58, 95, 205); // Blue
+
+            }
+        }
+        imageWriter.writeToimage();
+    }
+
+    @Test
+    public void writeImageFlagTest2() {
+        ImageWriter imageWriter = new ImageWriter("Image writer Libya flag test", 450, 300, 1, 1);
+        for (int i = 0; i < imageWriter.getHeight(); i++) {
+            for (int j = 0; j < imageWriter.getWidth(); j++) {
+                if (i >= 299 || i <= 1 || j >= 449 || j <= 1)
+                    imageWriter.writePixel(j, i, 105, 105, 105);
+                else
+                    imageWriter.writePixel(j, i, 60, 179, 113);
+            }
+        }
+        imageWriter.writeToimage();
+    }
+}
